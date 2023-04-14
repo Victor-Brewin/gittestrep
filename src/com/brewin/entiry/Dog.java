bc2480b063d206d73a1d5d5a91204967b702ceca
brewin <brewin@foxmail.com>
2023-04-14 16:47:36 +0800

    新增Dog

---
+++ src/com/brewin/entiry/Dog.java
@@ -0,0 +1,19 @@
+package com.brewin.entiry;
+
+import com.brewin.service.Action;
+
+public class Dog extends Animal implements Action {
+
+	public Dog(String name) {
+		super(name);
+		
+	}
+
+	@Override
+	public void bark() {
+		System.out.println(this.getName()+":ЭєЭєНа");
+
+		
+	}
+
+}
