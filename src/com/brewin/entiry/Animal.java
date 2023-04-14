c99d6ad466e56500af2a343f46930d50f616c104
brewin <brewin@foxmail.com>
2023-04-14 16:38:31 +0800

    init

---
+++ src/com/brewin/entiry/Animal.java
@@ -0,0 +1,20 @@
+package com.brewin.entiry;
+
+public class Animal {
+	String name;
+
+	public Animal(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+}
---
+++ src/com/brewin/service/CatService.java
@@ -0,0 +1,5 @@
+package com.brewin.service;
+
+public class CatService {
+
+}
